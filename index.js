09da663f20e1a7f540f2bbf6a37379367d6f2969
Lidya Ghilay <lidyahenok92gmail.com>
2024-04-03 11:39:42 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+    }
+    saturdayFun()
+    
+    function mondayWork (work = "go to the office"){
+    return `This Monday, I will ${work}.`
+    }
+    mondayWork()
+    function wrapAdjective(flair = "*"){
+        return function(me ="special" ){
+    return `You are ${flair}${me}${flair}!`
+        }
+    }
+    const encouragingPromptFunction =  wrapAdjective("!!!")
+    console.log(encouragingPromptFunction("amazing"));
